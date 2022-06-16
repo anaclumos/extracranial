67bb65e47d456df7fea6bce7e1eb857cf4222cf3
Sunghyun Cho <mail@chosunghyun.com>
2022-06-15 17:06:18 -0700

    Separate Config Files

---
+++ config/footer.config.ts
@@ -0,0 +1,9 @@
+import type { Footer } from '@docusaurus/theme-common'
+
+const footer: Footer = {
+  style: 'dark',
+  copyright: `Copyright © ${new Date().getFullYear()} Sunghyun Cho`,
+  links: [],
+}
+
+export = footer
