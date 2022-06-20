dbeed9e5bf1d44a756c7eef33381b70f8d03ddb5
Sunghyun Cho <mail@chosunghyun.com>
2022-06-19 18:27:40 -0700

    add comments

---
+++ src/components/Giscus/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Giscus from '@giscus/react'
+import styles from './index.module.css'
+
+const index = () => {
+  return (
+    <div className={styles.giscus}>
+      <Giscus
+        id='comments'
+        repo='anaclumos/www-comments'
+        repoId='R_kgDOHh2XAw'
+        category='General'
+        categoryId='DIC_kwDOHh2XA84CPxJo'
+        mapping='pathname'
+        reactionsEnabled='1'
+        emitMetadata='0'
+        inputPosition='top'
+        theme='preferred_color_scheme'
+        lang='en'
+      />
+    </div>
+  )
+}
+
+export default index
