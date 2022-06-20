3a336b7db2d1c799bb4d0436da073973abf26ded
Sunghyun Cho <mail@chosunghyun.com>
2022-06-19 18:01:37 -0700

    update

--- src/components/EmojiReplaceableText.tsx
+++ src/components/EmojiReplaceableText.tsx
@@ -32,7 +32,7 @@ const EmojiReplaceableText = (props: Props) => {
           photo === undefined ? (
             text
           ) : (
-            <img src={photo} alt={photoAlt} width='1.15rem' height='1.15rem' />
+            <img src={photo} alt={photoAlt} width='2rem' height='2rem' />
           )
         ) : (
           <span className={styles.emoji}>{emoji}</span>
