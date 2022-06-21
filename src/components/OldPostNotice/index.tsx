0f8b3df0c589011c462f9adf12fc6a3182b34d1f
Sunghyun Cho <mail@chosunghyun.com>
2022-06-20 17:21:48 -0700

    custom style for oldpostnotice

--- src/components/OldPostNotice/index.tsx
+++ src/components/OldPostNotice/index.tsx
@@ -47,11 +47,17 @@ const index = (props: Props) => {
   const { metadata } = props
   return (
     isOldPost(metadata.date) && (
-      <div className='admonition admonition-warning alert alert--danger'>
-        <div className='admonition-heading'>
+      <div className={styles.old}>
+        <div>
           <h5>
-            <span className='admonition-icon'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='12' height='16' viewBox='0 0 12 16'>
+            <span className={styles.iconspan}>
+              <svg
+                className={styles.icon}
+                xmlns='http://www.w3.org/2000/svg'
+                width='12'
+                height='16'
+                viewBox='0 0 12 16'
+              >
                 <path
                   fill-rule='evenodd'
                   d='M5.05.31c.81 2.17.41 3.38-.52 4.31C3.55 5.67 1.98 6.45.9 7.98c-1.45 2.05-1.7 6.53 3.53 7.7-2.2-1.16-2.67-4.52-.3-6.61-.61 2.03.53 3.33 1.94 2.86 1.39-.47 2.3.53 2.27 1.67-.02.78-.31 1.44-1.13 1.81 3.42-.59 4.78-3.42 4.78-5.56 0-2.84-2.53-3.22-1.25-5.61-1.52.13-2.03 1.13-1.89 2.75.09 1.08-1.02 1.8-1.86 1.33-.67-.41-.66-1.19-.06-1.78C8.18 5.31 8.68 2.45 5.05.32L5.03.3l.02.01z'
@@ -61,7 +67,7 @@ const index = (props: Props) => {
             <Translate>Old post ahead!</Translate>
           </h5>
         </div>
-        <div className='admonition-content'>
+        <div>
           <p>
             <ul>
               <li>
@@ -79,8 +85,8 @@ const index = (props: Props) => {
                 <Translate>I might not agree with this post anymore.</Translate>
               </li>
             </ul>
-            <SearchGoogleButton title={metadata.title} />
           </p>
+          <SearchGoogleButton title={metadata.title} />
         </div>
       </div>
     )
