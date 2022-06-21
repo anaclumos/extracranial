83ae2fd36d9570b422cc948f113efb52ba354918
Sunghyun Cho <mail@chosunghyun.com>
2022-06-21 00:37:56 -0700

    Automatic Commit.

---
+++ src/theme/BlogArchivePage/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="@docusaurus/plugin-content-blog" />
+/// <reference types="react" />
+import type { Props } from '@theme/BlogArchivePage';
+export default function BlogArchive({ archive }: Props): JSX.Element;
