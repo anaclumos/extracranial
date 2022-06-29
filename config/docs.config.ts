2ea4c0f851ce75f05f1d477226d2baf8e83b37dd
Sunghyun Cho <mail@chosunghyun.com>
2022-06-28 23:36:03 -0700

    add sidebar processor

--- config/docs.config.ts
+++ config/docs.config.ts
@@ -1,6 +1,24 @@
 const math = require('remark-math')
 const katex = require('rehype-katex')
 
+const sidebarProcessor = (items) => {
+  const result = items.map((item) => {
+    if (item.type === 'category') {
+      // set collapsible and collapsed to true
+      item.collapsible = true
+      item.collapsed = true
+      // if label is 'Journals', reverse the order of items
+      if (item.label === 'Journals') {
+        item.items = item.items.sort().reverse()
+      } else {
+        item.items = item.items.sort(() => Math.random() - 0.5)
+      }
+    }
+    return item
+  })
+  return result
+}
+
 const docs = {
   sidebarPath: require.resolve('./sidebars.js'),
   routeBasePath: '/research',
@@ -9,6 +27,10 @@ const docs = {
   sidebarCollapsible: true,
   remarkPlugins: [math],
   rehypePlugins: [katex],
+  async sidebarItemsGenerator({ defaultSidebarItemsGenerator, ...args }) {
+    const sidebarItems = await defaultSidebarItemsGenerator(args)
+    return sidebarProcessor(sidebarItems)
+  },
 }
 
 export = docs
