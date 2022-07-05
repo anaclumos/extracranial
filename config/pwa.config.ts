0ca08709af505bf26bd89a37a98ebe18b7e60114
Sunghyun Cho <mail@chosunghyun.com>
2022-07-05 00:37:25 -0700

    manage configs
    Former-commit-id: e4394229a87ad0f6ae88ee13cb5cb67d179692b3

---
+++ config/pwa.config.ts
@@ -0,0 +1,44 @@
+const pwaOptions = {
+  debug: false,
+  offlineModeActivationStrategies: ['appInstalled', 'queryString', 'standalone'],
+  pwaHead: [
+    {
+      tagName: 'link',
+      rel: 'icon',
+      href: '/img/favicon.png',
+    },
+    {
+      tagName: 'link',
+      rel: 'manifest',
+      href: '/manifest.json', // your PWA manifest
+    },
+    {
+      tagName: 'meta',
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes',
+    },
+    {
+      tagName: 'link',
+      rel: 'apple-touch-icon',
+      href: '/img/favicon.png',
+    },
+    {
+      tagName: 'link',
+      rel: 'mask-icon',
+      href: '/img/favicon.png',
+      color: '#5597ec',
+    },
+    {
+      tagName: 'meta',
+      name: 'msapplication-TileImage',
+      content: '/img/favicon.png',
+    },
+    {
+      tagName: 'meta',
+      name: 'msapplication-TileColor',
+      content: '#5597ec',
+    },
+  ],
+}
+
+export = pwaOptions
