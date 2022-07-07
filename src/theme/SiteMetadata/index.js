f57ed62093bd1c70934438b23dfc3021a64e6419
Sunghyun Cho <mail@chosunghyun.com>
2022-07-06 20:03:53 -0700

    naver site verification
    Former-commit-id: 6efc2095d8d3c17098beb8c1a247aba12d325f6f

--- src/theme/SiteMetadata/index.js
+++ src/theme/SiteMetadata/index.js
@@ -4,6 +4,7 @@ import SiteMetadata from '@theme-original/SiteMetadata'
 export default function SiteMetadataWrapper(props) {
   return (
     <>
+      <meta name='naver-site-verification' content='15b31306fd3391cd0bf411b1d49160aa02dd3cad' />
       <script async defer src='https://scripts.simpleanalyticscdn.com/latest.js'></script>
       <noscript>
         <img
