7afc20e994ddf95493fd6b18692ad04a4c77b3b9
Sunghyun Cho <mail@chosunghyun.com>
2022-07-10 14:27:32 -0700

    update github links
    Former-commit-id: 3526157358412a0dc96bbdd544b2ea3d32e8249f

--- config/blog.config.ts
+++ config/blog.config.ts
@@ -11,12 +11,7 @@ const blog = {
   remarkPlugins: [math],
   rehypePlugins: [katex],
   editUrl: ({ locale, blogDirPath, blogPath }) => {
-    if (blogPath.endsWith('.mdx')) {
-      return `https://github.com/anaclumos/www/tree/main/posts/${blogPath.replace('/index.mdx', '')}/${locale}.mdx`
-    }
-    if (blogPath.endsWith('.md')) {
-      return `https://github.com/anaclumos/www/tree/main/posts/${blogPath.replace('/index.md', '')}/${locale}.md`
-    }
+    return `https://github.com/anaclumos/www/tree/main/posts/${blogPath.replace('/index.md', '')}/${locale}.md`
   },
 }
 
