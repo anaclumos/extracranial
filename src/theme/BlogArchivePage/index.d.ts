6a59d5ffcb844cff4bc760af686cb7607e250a9f
Sunghyun Cho <mail@chosunghyun.com>
2022-07-10 14:10:24 -0700

    Upgrade to Docusaurus 2.b.22

--- src/theme/BlogArchivePage/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="@docusaurus/plugin-content-blog" />
-/// <reference types="react" />
-import type { Props } from '@theme/BlogArchivePage';
-export default function BlogArchive({ archive }: Props): JSX.Element;
