781cd52893edac11fa4fbbbaf7e883be6bcb196f
Sunghyun Cho <mail@chosunghyun.com>
2022-08-03 21:53:04 +0900

    trivial note update
    Former-commit-id: de907dd1fdff257e44ebe50697a249c166c9a4ae

---
+++ src/components/DisplayFlex/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './index.module.css'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const DisplayFlex = (props: Props) => {
+  return (
+    <div className={styles.displayFlex}>
+      {props.children}
+    </div>
+  )
+}
+
+export default DisplayFlex
