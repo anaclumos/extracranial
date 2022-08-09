e42903fcbc1cf9ba0146f8936136bf1afbf28944
Sunghyun Cho <mail@chosunghyun.com>
2022-08-10 00:14:03 +0900

    Add YouTube
    Former-commit-id: 109e8b1c57dbe3b93b24dcacaae3066906974f17

---
+++ src/components/YouTube/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './index.module.css'
+
+type Props = {
+  id: string
+}
+
+const YouTube = ({ id }: Props) => {
+  return (
+    <iframe
+      className={styles.youtube}
+      title="YouTube"
+      src={`https://www.youtube.com/embed/${id}`}
+    ></iframe>
+  )
+}
+
+export default YouTube
