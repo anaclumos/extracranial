e24aff1676d307ae953cf4485914977ccdd2d940
Sunghyun Cho <mail@chosunghyun.com>
2022-09-24 18:20:19 -0700

    adjust size for sidebar and toc

---
+++ src/theme/TOCCollapsible/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useCollapsible, Collapsible} from '@docusaurus/theme-common';
+import TOCItems from '@theme/TOCItems';
+import CollapseButton from '@theme/TOCCollapsible/CollapseButton';
+import type {Props} from '@theme/TOCCollapsible';
+
+import styles from './styles.module.css';
+
+export default function TOCCollapsible({
+  toc,
+  className,
+  minHeadingLevel,
+  maxHeadingLevel,
+}: Props): JSX.Element {
+  const {collapsed, toggleCollapsed} = useCollapsible({
+    initialState: true,
+  });
+  return (
+    <div
+      className={clsx(
+        styles.tocCollapsible,
+        !collapsed && styles.tocCollapsibleExpanded,
+        className,
+      )}>
+      <CollapseButton collapsed={collapsed} onClick={toggleCollapsed} />
+      <Collapsible
+        lazy
+        className={styles.tocCollapsibleContent}
+        collapsed={collapsed}>
+        <TOCItems
+          toc={toc}
+          minHeadingLevel={minHeadingLevel}
+          maxHeadingLevel={maxHeadingLevel}
+        />
+      </Collapsible>
+    </div>
+  );
+}
