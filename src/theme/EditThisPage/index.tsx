36e386c7a67098e6df2845acb77c4850d0397e8c
Sunghyun Cho <mail@chosunghyun.com>
2022-10-05 00:43:17 -0700

    adjust pixel values for link decorators

---
+++ src/theme/EditThisPage/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Translate from '@docusaurus/Translate'
+import { ThemeClassNames } from '@docusaurus/theme-common'
+import IconEdit from '@theme/Icon/Edit'
+import type { Props } from '@theme/EditThisPage'
+import clsx from 'clsx'
+import styles from './styles.module.css'
+
+export default function EditThisPage({
+  editUrl,
+}: Props): JSX.Element {
+  return (
+    <a
+      href={editUrl}
+      target="_blank"
+      rel="noreferrer noopener"
+      className={clsx(
+        'ThemeClassNames.common.editThisPage',
+        styles.editThisPage
+      )}
+    >
+      <IconEdit />
+      <Translate
+        id="theme.common.editThisPage"
+        description="The link label to edit the current page"
+      >
+        Edit this page
+      </Translate>
+    </a>
+  )
+}
