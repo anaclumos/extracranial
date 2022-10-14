bd9befb85b4ba71f02bf5c5a2b8c0b95538d41dd
Sunghyun Cho <mail@chosunghyun.com>
2022-10-14 16:41:59 -0700

    wrote something

--- Brain/.nota/hooks/paste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# @parameters clipboardText
-
-# the clipboard text
-clipboardText=$1
-
-# comment out the line below, modify $clipboardText, and call `printf` to insert the text in Nota
-# printf "%s" "$clipboardText"
