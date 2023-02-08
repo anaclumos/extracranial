ebac4a55ffd4e325db26cd7b43dbb04d1b563c0a
Sunghyun Cho <mail@chosunghyun.com>
2023-02-08 12:43:47 -0800

    try-catch errors

--- src/components/Backlink/index.tsx
+++ src/components/Backlink/index.tsx
@@ -19,22 +19,26 @@ const processBacklinkItem = (text: string, title: string) => {
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;')
 
-  // Replace [[title|display]] with `<b class="${styles.highlight}">${display}</b>` with regex
-  const regex1 = new RegExp(`\\[\\[${title}\\|(.+?)\\]\\]`, 'g')
-  text = text.replace(regex1, `<b class="${styles.highlight}">$1</b>`)
+  try {
+    // Replace [[title|display]] with `<b class="${styles.highlight}">${display}</b>` with regex
+    const regex1 = new RegExp(`\\[\\[${title}\\|(.+?)\\]\\]`, 'g')
+    text = text.replace(regex1, `<b class="${styles.highlight}">$1</b>`)
 
-  // Replace [[title]] with `<b class="${styles.highlight}">${title}</b>` with regex
-  const regex2 = new RegExp(`\\[\\[${title}\\]\\]`, 'g')
-  text = text.replace(regex2, `<b class="${styles.highlight}">${title}</b>`)
+    // Replace [[title]] with `<b class="${styles.highlight}">${title}</b>` with regex
+    const regex2 = new RegExp(`\\[\\[${title}\\]\\]`, 'g')
+    text = text.replace(regex2, `<b class="${styles.highlight}">${title}</b>`)
 
-  // Replace [[other text|display]] with display. other can include spaces
-  const regex3 = new RegExp(`\\[\\[(. +?)\\|(. +?)\\]\\]`, 'g')
-  text = text.replace(regex3, '$2')
+    // Replace [[other text|display]] with display. other can include spaces
+    const regex3 = new RegExp(`\\[\\[(. +?)\\|(. +?)\\]\\]`, 'g')
+    text = text.replace(regex3, '$2')
 
-  // Replace [[other]] with other
-  const regex4 = new RegExp(`\\[\\[(.+?)\\]\\]`, 'g')
-  text = text.replace(regex4, '$1')
-
+    // Replace [[other]] with other
+    const regex4 = new RegExp(`\\[\\[(.+?)\\]\\]`, 'g')
+    text = text.replace(regex4, '$1')
+  }
+  catch (e) {
+    console.warn(`Error processing backlink item with text: ${text} and title: ${title}\n${e}`)
+  }
 
   return (
     <pre
@@ -84,14 +88,14 @@ const Backlink = (props: Props) => {
                 </Link>
               )
             })) || (
-          <p className={styles.noBacklink}>
-            {translate({
-              id: 'backlink.noBacklink',
-              message: 'Nothing here yet...',
-              description: 'The message when there is no backlink',
-            })}
-          </p>
-        )}
+            <p className={styles.noBacklink}>
+              {translate({
+                id: 'backlink.noBacklink',
+                message: 'Nothing here yet...',
+                description: 'The message when there is no backlink',
+              })}
+            </p>
+          )}
       </div>
     </div>
   )
