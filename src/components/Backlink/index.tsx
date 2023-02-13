188b4dc8fe5fb3698e2128dbc8ceea063586f758
Sunghyun Cho <mail@chosunghyun.com>
2023-02-12 16:32:44 -0800

    📝 wrote something

--- src/components/Backlink/index.tsx
+++ src/components/Backlink/index.tsx
@@ -36,7 +36,7 @@ const processBacklinkItem = (text: string, title: string) => {
     const regex4 = new RegExp(`\\[\\[(.+?)\\]\\]`, 'g')
     text = text.replace(regex4, '$1')
   } catch (e) {
-    console.warn(`Error processing backlink item with text: ${text} and title: ${title}\n${e}`)
+    // console.warn(`Error processing backlink item with text: ${text} and title: ${title}\n${e}`)
   }
 
   return (
