f48310850d68ec0061b319b3d07f89d9bd71b919
Sunghyun Cho <mail@chosunghyun.com>
2023-03-07 20:03:05 -0800

    add newsletter

---
+++ src/theme/Icon/ExternalLink/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import type { Props } from '@theme/Icon/ExternalLink';
+
+import styles from './styles.module.css';
+
+export default function IconExternalLink({
+  width = 13.5,
+  height = 13.5,
+}: Props): JSX.Element {
+  return (
+    <svg
+      width={width}
+      height={height}
+      xmlns='http://www.w3.org/2000/svg'
+      className={styles.iconExternalLink}
+      viewBox='0 0 64 64'>
+    </svg>
+  );
+}
