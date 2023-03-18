8fa5ef5a4e0b6828361e7ac58bc183958e1b086e
Sunghyun Cho <mail@chosunghyun.com>
2023-03-18 01:43:52 -0700

    Add support for Resend notifications and collect device info for newsletters job

--- newsletters/newsletters.py
+++ newsletters/newsletters.py
@@ -4,13 +4,17 @@ import requests
 import datetime
 import os
 import frontmatter
+from pytz import timezone
 from dotenv import load_dotenv
+from resend import Resend  # for notification
 
 load_dotenv()
 
 server = "https://newsletters.cho.sh/api/campaigns"
 username = os.environ["NEWSLETTERS_CHO_SH_USERNAME"]
 password = os.environ["NEWSLETTERS_CHO_SH_PASSWORD"]
+resend = Resend(os.environ["RESEND_KEY"])  # Just for me
+notification = ""
 
 CONFIG = {
     "ko": {
@@ -58,6 +62,9 @@ def create_campaign(title, body, lang="en"):
 def find_today_newsletters(lang):
     """Find the newsletter for today in the Research folder. All UTC."""
     today = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+    tomorrow = (datetime.datetime.utcnow() + datetime.timedelta(days=1)).strftime(
+        "%Y-%m-%d"
+    )
     all_md_files = []
     newsletters = []
     for root, _, files in os.walk("./Research/pages"):
@@ -69,12 +76,51 @@ def find_today_newsletters(lang):
             post = frontmatter.load(f)
         if (
             "newsletter" in post.keys()
-            and post["newsletter"] == today
+            and (post["newsletter"] == today or post["newsletter"] == tomorrow)
             and post["lang"] == lang
         ):
             title = ".".join(md_file.split("/")[-1].split(".")[:-1])
             newsletters.append((title, post.content))
     return newsletters
+
+
+def collect_device_info():
+    import platform
+    import psutil
+
+    collect_notification("\n\n\nCollecting device information...")
+    uname = platform.uname()
+    collect_notification(f"System: {uname.system}")
+    collect_notification(f"Node Name: {uname.node}")
+    collect_notification(f"Release: {uname.release}")
+    collect_notification(f"Version: {uname.version}")
+    collect_notification(f"Machine: {uname.machine}")
+    collect_notification(f"Processor: {uname.processor}")
+    collect_notification("Physical cores:", psutil.cpu_count(logical=False))
+    collect_notification("Total cores:", psutil.cpu_count(logical=True))
+    cpufreq = psutil.cpu_freq()
+    collect_notification(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+    collect_notification(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+    collect_notification(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+    collect_notification(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+
+def notify(title, text):
+    """Notify me about newsletter results."""
+    resend.send_email(
+        sender="notification@resend.dev",
+        to="hey@cho.sh",
+        subject=title,
+        text=text,
+    )
+
+
+def collect_notification(*args):
+    args = [str(arg) for arg in args]
+    text = " ".join(args)
+    """Collect notifications and send them at the end."""
+    global notification
+    notification += text + "\n"
 
 
 def schedule_newsletter(lang):
@@ -86,14 +132,20 @@ def schedule_newsletter(lang):
             duplicate = False
             for campaign in campaigns:
                 if title in campaign["name"]:
-                    print("Duplicate...", title)
+                    collect_notification("Duplicate... ", title)
                     duplicate = True
             if not duplicate:
-                print("Scheduling...", title)
+                collect_notification("Scheduling... ", title)
                 create_campaign(title, post, lang)
     else:
-        print("No newsletter for today.")
+        collect_notification("No newsletter for today.")
 
 
 schedule_newsletter("en")
 schedule_newsletter("ko")
+collect_device_info()
+
+notify(
+    f"Newsletter Job {datetime.datetime.now().astimezone(timezone('US/Pacific')).strftime('%Y-%m-%d %H:%M:%S')}",
+    notification,
+)
