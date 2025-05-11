2b7f5d43c10c3a19e7a60ba87310f656afeab2bf
Sunghyun Cho <hey@cho.sh>
2025-05-11 17:22:13 +0900

    feat: Add highlight styles for popover component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/util/cn"
+import { cn } from "@site/src/util/cn"
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
