6e78a2b0df20fadfcd6ccda5e43ed532b4d16d60
Sunghyun Cho <hey@cho.sh>
2025-05-12 00:22:47 +0900

    style: Update JSX import and add className to header

--- src/theme/BlogArchivePage/index.tsx
+++ src/theme/BlogArchivePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useId } from 'react'
+import React, { JSX, useId } from 'react'
 import Link from '@docusaurus/Link'
 import { translate } from '@docusaurus/Translate'
 import { PageMetadata } from '@docusaurus/theme-common'
