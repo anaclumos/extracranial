e09d002f0b683fa0c42db7882ffd4da5cf26961d
Sunghyun Cho <hey@cho.sh>
2025-05-11 17:45:14 +0900

    wrote something

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -2,11 +2,7 @@
 
 import { type ReactNode, useMemo } from 'react'
 
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@site/src/components/ui/popover'
+import { Popover, PopoverContent, PopoverTrigger } from '@site/src/components/ui/popover'
 import { cn } from '@site/src/util/cn'
 import { Drawer } from 'vaul'
 
@@ -18,21 +14,9 @@ interface HighlightProps {
   direction?: 'bottom' | 'top' | 'left' | 'right'
 }
 
-const highlightClasses = [
-  'highlight-1',
-  'highlight-2',
-  'highlight-3',
-  'highlight-4',
-  'highlight-5',
-  'highlight-6',
-]
+const highlightClasses = ['highlight-1', 'highlight-2', 'highlight-3', 'highlight-4', 'highlight-5', 'highlight-6']
 
-export const Highlight = ({
-  trigger,
-  title,
-  content,
-  direction = 'bottom',
-}: HighlightProps) => {
+export const Highlight = ({ trigger, title, content, direction = 'bottom' }: HighlightProps) => {
   const highlightClass = useMemo(() => {
     const titleString = trigger?.toString() || ''
     // Use a more deterministic hashing algorithm
@@ -49,58 +33,32 @@ export const Highlight = ({
 
   return (
     <>
-      <span className='hidden md:inline-block'>
+      <span className="hidden md:inline-block">
         <Popover>
-          <PopoverTrigger
-            className={cn(
-              'cursor-pointer inline-block relative',
-              highlightClass,
-            )}
-            type='button'
-          >
+          <PopoverTrigger className={cn('cursor-pointer inline-block relative', highlightClass)} type="button">
             {trigger}
           </PopoverTrigger>
-          <PopoverContent
-            className='w-96 md:w-96'
-            side={direction}
-            align='center'
-          >
-            <div className='space-y-2'>
-              {title &&
-                (typeof title === 'string' ? (
-                  <h4 className='text-lg font-bold'>{title}</h4>
-                ) : (
-                  title
-                ))}
+          <PopoverContent className="w-96 md:w-96" side={direction} align="center">
+            <div className="space-y-2">
+              {title && (typeof title === 'string' ? <h4 className="text-lg font-bold">{title}</h4> : title)}
               {content}
             </div>
           </PopoverContent>
         </Popover>
       </span>
-      <span className='md:hidden'>
+      <span className="md:hidden">
         <Drawer.Root>
-          <Drawer.Trigger
-            className={cn(
-              'cursor-pointer inline-block relative',
-              highlightClass,
-            )}
-            type='button'
-          >
+          <Drawer.Trigger className={cn('cursor-pointer inline-block relative', highlightClass)} type="button">
             {trigger}
           </Drawer.Trigger>
           <Drawer.Portal>
-            <Drawer.Overlay className='fixed inset-0 bg-black/40' />
-            <Drawer.Content className='bg-background h-fit fixed bottom-0 left-0 right-0 outline-none p-4 pb-12'>
-              <div
-                aria-hidden
-                className='mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-2'
-              />
-              <div className='space-y-2'>
+            <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+            <Drawer.Content className="bg-background h-fit fixed bottom-0 left-0 right-0 outline-none p-4 pb-12">
+              <div aria-hidden className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-2" />
+              <div className="space-y-2">
                 {title &&
                   (typeof title === 'string' ? (
-                    <Drawer.Title className='text-lg font-bold'>
-                      {title}
-                    </Drawer.Title>
+                    <Drawer.Title className="text-lg font-bold">{title}</Drawer.Title>
                   ) : (
                     title
                   ))}
