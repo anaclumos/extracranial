d1d6c2340407c662a4b8f7863477f1b315e93c30
Sunghyun Cho <hey@cho.sh>
2025-09-05 22:49:59 +0900

    Enhance YouTube component with responsive styling
    - Added class names to the iframe in the YouTube component for improved aspect ratio and full-width display.

--- src/components/youtube.tsx
+++ src/components/youtube.tsx
@@ -1,3 +1,3 @@
 export default function YouTube({ id }: { id: string }) {
-  return <iframe src={`https://www.youtube.com/embed/${id}`} allowFullScreen />
+  return <iframe className="aspect-video w-full" src={`https://www.youtube.com/embed/${id}`} allowFullScreen />
 }
