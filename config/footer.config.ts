45e140181a9fb8f7da823501a37ef2454bef91f6
Sunghyun Cho <hey@cho.sh>
2026-01-01 21:30:33 +0900

    chore: remove deprecated configuration files and Python version specification

--- config/footer.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Footer } from '@docusaurus/theme-common'
-
-const footer: Footer = {
-  style: 'dark',
-  copyright: `© ${new Date().getFullYear()} Sunghyun Cho`,
-  links: [],
-}
-
-export default footer
