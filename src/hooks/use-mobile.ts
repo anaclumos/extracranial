eec54b38fdc6af65af39bbf742f92b970310c30b
Sunghyun Cho <hey@cho.sh>
2026-03-17 23:16:14 +0900

    chore: remove unused files and dependencies

--- src/hooks/use-mobile.ts
+++
@@ -1,7 +0,0 @@
-import { useMediaQuery } from "./use-media-query";
-
-const MOBILE_BREAKPOINT = 768;
-
-export function useIsMobile() {
-  return useMediaQuery(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-}
